f78e1472b4e3c5b6579e9308c96a87d451bb4cc3
Malik Moss <malikmoss33@gmail.com>
2021-11-19 01:28:35 -0600

    test

---
+++ test/script-spec.js
@@ -0,0 +1,13 @@
+const assert = require("assert");
+const { DESCRIBE } = require("sequelize/types/lib/query-types");
+// this is a relative path to the function's location
+const script = require("../script.js");
+
+describe("grabMovies()", function() {
+    it('should return  the favorite movie of a person, their friends, and friends of their friends', function (){
+        let test = grabMovies({Shrek: 1, Avengers: 3, HarryPotter: 5})
+        let result = HarryPotter
+
+        assert.strictEqual(test, result)
+    })
+})
